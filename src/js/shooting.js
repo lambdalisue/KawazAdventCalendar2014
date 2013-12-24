db2a4a8df5f95d9e2891b3091ffe8671576c0350
Λlisue <lambdalisue@hashnote.net>
2013-12-24 23:31:54 +0900

    Hello World 変数編

--- src/js/shooting.js
+++ src/js/shooting.js
@@ -1,4 +1,14 @@
 "use strict"
+// 変数 message を定義
+var message;
+// 変数 message に「文字列」を代入
+message = "Hello World";
 
 // デベロッパーツールにログとして表示
-console.log("Hello World");
+console.log(message);
+
+// 変数 number を定義して「文字列」を代入
+var message2 = "ハローワールド";
+
+// デベロッパーツールにログとして表示
+console.log(message2);
